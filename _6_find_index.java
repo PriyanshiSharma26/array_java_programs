0993155d63efb41a17646342d44c9bbea057d7e6
PriyanshiSharma26 <priyanshisharma8220708@gmail.com>
2024-08-23 09:54:21 -0700

    Add files via upload

---
+++ _6_find_index.java
@@ -0,0 +1,22 @@
+//find out the index position of an element
+
+
+public class _6_find_index
+ {
+    public static void main(String[] args)
+     {
+       int arry[]={10,20,30,40} ;
+       int i;
+       for(i=0;i<arry.length;i++)
+       {
+        if(arry[i]==40)
+        {
+            System.out.println("index position of  "+ arry[i]+" is: " +i);
+            break;
+        }
+    }
+
+       }
+    }
+    
+
